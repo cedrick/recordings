404320c669924f4dc9aee0aa4b58894f676650e1
cedrick macatangay <cedrickmacatangay@gmail.com>
2012-01-09 20:55:50 +0800

    templates

---
+++ templates/footer.php
@@ -0,0 +1,7 @@
+<div class="footer" align="center">
+	<font color="#A0A0A4">
+		NorthStar Solutions INC. &nbsp;|&nbsp; All Rights Reserved @ 2011&nbsp;|&nbsp; I.T. Software Developer&nbsp;| &nbsp;Cedrick|Ambet		 		&nbsp;|&nbsp; Privacy Policy & Terms of Use&nbsp;|&nbsp;Online&nbsp;Recording&nbsp;Ver.&nbsp;2.0.0&nbsp;|
+  </font>
+</div>
+</body>
+</html>
